4b76a02e8800fbc6d3c507a29f238338b9448bc5
yogeeswar-chekkera <yogeeswarchekkera@gmail.com>
2026-02-17 13:48:02 +0530

    Refactor OOPSBannerApp to OopsBannerUC3

---
+++ src/OopsBannerUC3.java
@@ -0,0 +1,15 @@
+public class OopsBannerUC3{
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ", "    ***   ", "    ***   ", "*****  ", "    *****  "));
+        System.out.println(String.join(" ", "  **   **", "   **   ** ", "**   **", "  **      "));
+        System.out.println(String.join(" ", " **     **", " **     **", "**   **", " **      "));
+        System.out.println(String.join(" ", " **     **", " **     **", "**   **", "  **      "));
+        System.out.println(String.join(" ", " **     **", " **     **", "*****  ", "    ****   "));
+        System.out.println(String.join(" ", " **     **", " **     **", "**     ", "        **  "));
+        System.out.println(String.join(" ", " **     **", " **     **", "**     ", "         **  "));
+        System.out.println(String.join(" ", "  **   **", "   **   ** ", "**     ", "        **  "));
+        System.out.println(String.join(" ", "    ***   ", "    ***   ", "**     ", "   *****   "));
+    }
+}
