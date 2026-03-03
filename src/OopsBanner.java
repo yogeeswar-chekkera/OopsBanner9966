24c79eab82b057eda17eaf13d159cded7f8d0d4b
Yogeeswar Chekkera <yogeeswarchekkera@gmail.com>
2026-03-03 18:27:39 +0530

    Delete src directory

--- src/OopsBanner.java
+++
@@ -1,5 +0,0 @@
-public class OopsBanner{
-    public static void main(String[] args){
-        System.out.println("Welcome to Oops Banner ...");
-    }
-}
--- src/OopsBannerUC1.java
+++
@@ -1,5 +0,0 @@
-public class OOPSBannerApp {
-	public static void main(String[] args) {
-		system.out.print("OOPS");
-	}
-}
