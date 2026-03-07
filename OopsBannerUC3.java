ce572034248542bc31832fdc9d8a467dc03829f9
Yogeeswar Chekkera <yogeeswarchekkera@gmail.com>
2026-03-07 13:35:04 +0530

    Add OOPSBannerApp to display a banner

---
+++ OopsBannerUC3.java
@@ -0,0 +1,15 @@
+public class OOPSBannerApp{
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ", "    ***   ", "    ***   ", "*****  ", "    *****  "));
+        System.out.println(String.join(" ", "  **   **", "   **   ** ", "**   **", "  **      "));
+        System.out.println(String.join(" ", " **     **", " **     **", "**   **", " **      "));
+        System.out.println(String.join(" ", " **     **", " **     **", "**   **", "  **      "));
+        System.out.println(String.join(" ", " **     **", " **     **", "*****  ", "    ****   "));
+        System.out.println(String.join(" ", " **     **", " **     **", "**     ", "        **  "));
+        System.out.println(String.join(" ", " **     **", " **     **", "**     ", "         **  "));
+        System.out.println(String.join(" ", "  **   **", "   **   ** ", "**     ", "        **  "));
+        System.out.println(String.join(" ", "    ***   ", "    ***   ", "**     ", "   *****   "));
+    }
+}
